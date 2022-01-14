9725daf1c2c7ce0898fe9bd7ae221c9d1f346a1d
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-14 13:58:21 +0200

    Add withdraw route

--- routes/users/users-routes.js
+++ routes/users/users-routes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const fs = require("fs");
-const dbPath = path.join(__dirname, "../../db", "db.json");
-
 const addUser = require("./controllers/addUser");
 const getAllUsers = require("./controllers/getAllUsers");
 const getUser = require("./controllers/getUser");
 const makeDeposit = require("./controllers/makeDeposit");
 const updateUserCredit = require("./controllers/updateUserCredit");
+const toWithdraw = require("./controllers/toWithdraw");
 
 // Add user
 router.route("/add").post(addUser);
@@ -24,5 +21,8 @@ router.route("/:id/deposit").put(makeDeposit);
 
 // Update credit
 router.route("/:id/credit").put(updateUserCredit);
+
+// Withdraw money
+router.route("/:id/withdraw").put(toWithdraw);
 
 module.exports = router;
