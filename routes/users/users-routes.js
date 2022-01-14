087738281d8437c9b871b6bb81e4ac00a1d8a2ba
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-14 12:50:47 +0200

    Add controllers folder to routs for moduling

--- routes/users/users-routes.js
+++ routes/users/users-routes.js
@@ -2,31 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
-const dbPath = path.join(__dirname, "..", "db", "db.json");
+const dbPath = path.join(__dirname, "../../db", "db.json");
+const addUser = require("./controllers/addUser");
 
 // Add user
-router.route("/add").post((req, res) => {
-  fs.readFile(dbPath, "utf8", (err, data) => {
-    if (err) {
-      res.send(err.message);
-    }
-    const dataObject = JSON.parse(data);
-    const usersObject = dataObject.users;
-    const { id, cash, credit } = req.body;
-
-    if (!usersObject.hasOwnProperty(id)) {
-      usersObject[id] = { cash: +cash, credit: +credit };
-      fs.writeFile(dbPath, JSON.stringify(dataObject), (err) => {
-        if (err) {
-          res.send(err.message);
-        }
-        res.send(JSON.stringify(usersObject));
-      });
-    } else {
-      res.send("User already exists.");
-    }
-  });
-});
+router.route("/add").post(addUser);
 
 // Send details of all users
 router.route("/all").get((req, res) => {
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/user-routes');
-const path = require('path');
+const usersRoutes = require('./routes/users/users-routes');
 
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/users', userRoutes);
+app.use('/users', usersRoutes);
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
