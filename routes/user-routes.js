78bab8f3162e8a85f21990604ba4a99233bf6f6e
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-14 12:37:10 +0200

    Add depositing route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,45 +1,88 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const data = require("../db/db.json");
 const fs = require("fs");
+const dbPath = path.join(__dirname, "..", "db", "db.json");
 
 // Add user
 router.route("/add").post((req, res) => {
-  fs.readFile(
-    path.join(__dirname, "..", "db", "users.json"),
-    "utf8",
-    (err, data) => {
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      res.send(err.message);
+    }
+    const dataObject = JSON.parse(data);
+    const usersObject = dataObject.users;
+    const { id, cash, credit } = req.body;
+
+    if (!usersObject.hasOwnProperty(id)) {
+      usersObject[id] = { cash: +cash, credit: +credit };
+      fs.writeFile(dbPath, JSON.stringify(dataObject), (err) => {
+        if (err) {
+          res.send(err.message);
+        }
+        res.send(JSON.stringify(usersObject));
+      });
+    } else {
+      res.send("User already exists.");
+    }
+  });
+});
+
+// Send details of all users
+router.route("/all").get((req, res) => {
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      res.send(err.message);
+    }
+    res.send(JSON.parse(data));
+  });
+});
+
+// Send details of specific user
+router.route("/:id")
+  .get((req, res) => {
+    fs.readFile(dbPath, "utf8", (err, data) => {
       if (err) {
         res.send(err.message);
       }
       const dataObject = JSON.parse(data);
-      const usersObject = dataObject.users;
-      const { id, cash, credit } = req.body;
+      const { id } = req.params;
 
-      if (!usersObject.hasOwnProperty(id)) {
-        usersObject[id] = { cash: +cash, credit: +credit };
-        fs.writeFile(
-          path.join(__dirname, "..", "db", "users.json"),
-          JSON.stringify(dataObject),
-          (err) => {
-            if (err) {
-              res.send(err.message);
-            }
-            res.send(JSON.stringify(usersObject));
-          }
-        );
-      } else {
-        res.send("User already exists.");
-      }
-    }
-  );
+      res.send(dataObject.users[id]);
+    });
 });
 
-// Show details of all users
-router.route("/all").get((req, res) => {
-  const users = data.users;
-  res.json(users);
+// Depositing
+router.route("/:id/deposit")
+  .put((req, res) => {
+    if (req.body.hasOwnProperty('deposit')) {
+      const { deposit } = req.body;
+      if (+deposit > 0) {
+        
+        fs.readFile(dbPath, "utf8", (err, data) => {
+          if (err) {
+            res.send(err.message);
+          }
+          const dataObject = JSON.parse(data);
+          const usersObject = dataObject.users;
+          const { id } = req.params;
+      
+          if (usersObject.hasOwnProperty(id)) {
+            usersObject[id].cash += +deposit;
+            fs.writeFile(dbPath, JSON.stringify(dataObject), (err) => {
+              if (err) {
+                res.send(err.message);
+              }
+              res.send(JSON.stringify(usersObject[id]));
+            });
+          } else {
+            res.send(`There is no user with id: ${id}.`);
+          }
+        });
+      } else {
+        res.send(`Deposit must be positive.`);
+      }
+    }
 });
 
 module.exports = router;
